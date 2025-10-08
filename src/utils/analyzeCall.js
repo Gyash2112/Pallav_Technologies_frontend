7894a49cdd52095315efc8aefc51dda431561ea4
Yash Gupta <yash.gupta2@unthinkable.co>
2025-10-08 16:55:15 +0530

    Added correct url

--- src/utils/analyzeCall.js
+++ src/utils/analyzeCall.js
@@ -2,10 +2,13 @@ export const analyzeCall = async (file) => {
   const formData = new FormData();
   formData.append('audio', file);
 
-  const res = await fetch('https://pallav-techologies-backend.onrender.com', {
-    method: 'POST',
-    body: formData,
-  });
+  const res = await fetch(
+    'https://pallav-techologies-backend.onrender.com/api/analyze-call',
+    {
+      method: 'POST',
+      body: formData,
+    }
+  );
 
   if (!res.ok) throw new Error('Failed to analyze');
   return await res.json();
